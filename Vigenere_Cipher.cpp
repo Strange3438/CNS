2e5ef7e47d5426fce3297ce7d9534a022b19bd4a
Rahul Pragada <117922719+rahulpragada@users.noreply.github.com>
2024-01-31 12:29:03 +0530

    Update CNS

--- Vigenere_Cipher.cpp
+++ Vigenere_Cipher.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
+
 using namespace std;
 
 void Encryption(string plain_text, string key, string cipher_text){
     
     int x;
     cout << ">>> Encrypting the message <<<" << endl;
-    for (x = 0; x < plain_text.length(); x ++){
+    
+    for (x = 0; x < plain_text.length(); x ++) {
+        
         char a = ( plain_text[x] + key[x % key.length()] ) % 26;
         a += 'A';
         cipher_text.push_back(a);
+    
     }
+    
     cout << "Cipher Text : " << cipher_text << endl;
     
 }
@@ -18,21 +23,27 @@ void Decryption(string plain_text, string key, string cipher_text){
     
     int y;
     cout << ">>> Decrypting the message <<<" << endl;
-    for (y = 0; y < cipher_text.length(); y ++){
+    
+    for (y = 0; y < cipher_text.length(); y ++) {
+        
         char b = ( cipher_text[y] - key[y % key.length()] + 26) % 26;
         b += 'A';
         plain_text.push_back(b);
+    
     }
+    
     cout << "Plain Text : " << plain_text << endl;
     
 }
 
 int main(){
+    
     string plain_text, key, cipher_text;
     
     int choice;
     cout << "-----  Vigenere Cipher  -----" << endl;
-    while(true){
+    
+    while(true) {
         
         cipher_text = "";
         plain_text = "";
@@ -42,30 +53,33 @@ int main(){
         cout << "2. Decryption" << endl;
         cout << "3. Exit" << endl;
         cout << "Enter your option : ";
-        cin >> choice;
         
-        if (choice == 1) {
+        cin >> option;
+        
+        if (option == 1) {
             
-            cout << "Enter the Plain Text : ";
+            cout << "Enter the Plain Text (Uppercase only) : ";
             cin >> plain_text;
-            cout << "Enter the Key : ";
+            cout << "Enter the key : ";
             cin >> key;
             Encryption(plain_text, key, cipher_text);
             
         }
         
-        else if (choice == 2) {
+        else if (option == 2) {
             
-            cout << "Enter the Cipher Text : ";
+            cout << "Enter the Cipher Text (Uppercase only): ";
             cin >> cipher_text;
-            cout << "Enter the Key : ";
+            cout << "Enter the key : ";
             cin >> key;
             Decryption(plain_text, key, cipher_text);
             
         }
         
         else {
+            
             exit(0);
+            
         }
 
     }
